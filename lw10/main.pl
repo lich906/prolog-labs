759607a216553a063228aa8f431b68cb8b6a6825
AndreyM <222mushkin@gmail.com>
2024-02-22 12:05:46 +0300

    Исправил замечания по lw10

--- lw10/main.pl
+++ lw10/main.pl
@@ -15,39 +15,19 @@ vertical(N):-
     seg(N, point(X, _), point(X, _)).
 
 cross(N, M, point(X, Y), NL, ML) :-
-    seg(N, point(XH1, YH), point(XH2, YH)),
-    seg(M, point(XV, YV1), point(XV, YV2)),
-    XH1 =< XV, XV =< XH2,
-    YV1 >= YH, YH >= YV2,
-    X = XV,
-    Y = YH,
+    seg(N, point(XH1, Y), point(XH2, Y)),
+    seg(M, point(X, YV1), point(X, YV2)),
+    XH1 =< X, X =< XH2,
+    YV1 >= Y, Y >= YV2,
     ML is abs(YV2 - YV1),
     NL is abs(XH2 - XH1).
 
-cross(M, N, point(X, Y), NL, ML) :-
-    seg(N, point(XH1, YH), point(XH2, YH)),
-    seg(M, point(XV, YV1), point(XV, YV2)),
-    XH1 =< XV, XV =< XH2,
-    YV1 >= YH, YH >= YV2,
-    X = XV,
-    Y = YH,
-    ML is abs(YV2 - YV1),
-    NL is abs(XH2 - XH1).
-
-%crossBoth(N, M, point(X, Y), NL, ML) :-
-%  (cross(N, M, point(X1, Y1), NL1, ML1), X is X1, Y is Y1, NL is NL1, ML is ML1);
-%  (cross(M, N, point(X2, Y2), NL2, ML2), X is X2, Y is Y2, NL is NL2, ML is ML2).
-
-perimeter(A,B,C,D,P,S) :-
-  seg(A, point(_, AY), point(_, AY)),
-  seg(C, point(_, CY), point(_, CY)),
-  seg(B, point(BX, _), point(BX, _)),
-  seg(D, point(DX, _), point(DX, _)),
-  AY > CY,
-  BX > DX,
-  cross(A, B, point(ABX, ABY), _, _),
-  cross(B, C, point(BCX, BCY), _, _),
-  cross(C, D, point(CDX, CDY), _, _),
-  cross(D, A, point(DAX, DAY), _, _),
-  P is ((ABX-DAX)+(ABY-BCY)+(BCX-CDX)+(DAY-CDY)),
-  S is ((ABX-DAX)*(ABY-BCY)).
+perimetr(A, B, C, D, P, S) :-
+  cross(A, B, point(X1, Y2), _, _),
+  cross(C, B, point(X1, Y1), _, _),
+  cross(C, D, point(X2, Y1), _, _),
+  cross(A, D, point(X2, Y2), _, _),
+  X1 < X2,
+  Y1 < Y2,
+  P is 2*((X2 - X1)+(Y2 - Y1)),
+  S is (X2 - X1)*(Y2 - Y1).
